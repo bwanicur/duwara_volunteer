ab8a70738a8f1b88fef7d72e8461499219feb7d0
Ben Wanicur <bwanicur@gmail.com>
2020-12-04 17:10:42 -0800

    Implements simple idea for events index UI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,16 +6,18 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 
-import 'css/duwara_base'
+import "@fortawesome/fontawesome-free/js/all";
+
+import "../css/duwara_base"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
-import "controllers"
+// import "controllers"
 
 
 
